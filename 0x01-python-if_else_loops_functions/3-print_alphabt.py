b640ac925da19abe263bd822ad74cdb9b8a6ba01
A-fethi <fethiabderrahmane1@gmail.com>
2023-06-06 06:34:19 -0700

    fix multiple loops

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 101):
-    print("{:c}".format(i), end="")
-for i in range(102, 113):
-    print("{:c}".format(i), end="")
-for i in range(114, 123):
+for i in range(97, 123):
+    if i == 101 or i == 113:
+        continue
     print("{:c}".format(i), end="")

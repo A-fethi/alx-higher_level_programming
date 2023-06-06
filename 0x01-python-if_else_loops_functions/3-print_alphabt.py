24c15fce6b47e88781c3c6d46602aebbbbfc8c06
A-fethi <fethiabderrahmane1@gmail.com>
2023-06-06 06:17:54 -0700

    exclude e and q from printing

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(97, 101):
+    print("{:c}".format(i), end="")
+for i in range(102, 113):
+    print("{:c}".format(i), end="")
+for i in range(114, 123):
+    print("{:c}".format(i), end="")

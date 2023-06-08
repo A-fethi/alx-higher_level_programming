97964f86aa5be7af77bbf76a9f9acf2f3f8967c6
A-fethi <fethiabderrahmane1@gmail.com>
2023-06-08 10:30:52 -0700

    Who are you?

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main__":
+    names = dir(hidden_4)
+    for i in range(len(names)):
+        if names[i][0] != "_":
+            print(names[i])

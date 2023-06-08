75d742f5af35ef9d03879b389695cfc8ce26f77e
A-fethi <fethiabderrahmane1@gmail.com>
2023-06-08 09:23:29 -0700

    print argument in case of 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,13 @@ from sys import argv
 
 if __name__ == "__main__":
     args = len(argv)
-    if args == 0:
+    if args == 1:
         print("{}".format(args - 1), "arguments.")
-    elif args == 1:
+    elif args == 2:
         print("{}".format(args - 1), "argument:")
-    elif args > 1:
+        for i in range(1, args):
+            print("{}".format(i) + ":", argv[i])
+    elif args > 2:
         print("{}".format(args - 1), "arguments:")
         for i in range(1, args):
             print("{}".format(i) + ":", argv[i])

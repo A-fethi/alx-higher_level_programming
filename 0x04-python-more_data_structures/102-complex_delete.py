d8692c9582bf8c10baf17048d4bef11ea0cefebb
A-fethi <fethiabderrahmane1@gmail.com>
2023-06-14 12:10:19 -0700

    what was that print do there -_-

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,7 +3,7 @@ def complex_delete(a_dictionary, value):
     keys_to_remove = []
     for key, val in a_dictionary.items():
         if val == value:
-            print(keys_to_remove.append(key))
+            keys_to_remove.append(key)
     for i in keys_to_remove:
         a_dictionary.pop(i)
     return a_dictionary

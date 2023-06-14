b4528e80cd3d10129f1120e67613b2b6366fa908
A-fethi <fethiabderrahmane1@gmail.com>
2023-06-14 12:00:21 -0700

    Delete by value

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    keys_to_remove = []
+    for key, val in a_dictionary.items():
+        if val == value:
+            print(keys_to_remove.append(key))
+    for i in keys_to_remove:
+        a_dictionary.pop(i)
+    return a_dictionary

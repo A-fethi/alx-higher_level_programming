7dedf882b363c87380d72a136208cb23b60e7568
A-fethi <fethiabderrahmane1@gmail.com>
2023-06-26 04:32:25 -0700

    add a counter to fix the except error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for counter in range(x):
+    counter = 0
+    for i in range(x):
         try:
-            print("{}".format(my_list[counter]), end="")
+            print("{}".format(my_list[i]), end="")
+            counter += 1
         except IndexError:
-            print()
-            return x - 2
+            break
     print()
-    return x
+    return counter

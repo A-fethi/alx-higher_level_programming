f51b11d483b4920303e26982b8c07a98d18c9c07
A-fethi <fethiabderrahmane1@gmail.com>
2023-06-26 04:26:31 -0700

    using format to print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     for counter in range(x):
         try:
-            print(my_list[counter], end="")
+            print("{}".format(my_list[counter]), end="")
         except IndexError:
             print()
             return x - 2

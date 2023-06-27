bf12c6bc300e68fda9ab77fdb9f60214532f788f
A-fethi <fethiabderrahmane1@gmail.com>
2023-06-27 06:21:02 -0700

    using private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A class representing an object with size attribute."""
+
+
+class Square:
+    def __init__(self, size):
+        """
+        Initialize the Square object with a given size.
+
+        Parameters:
+            size : The size of the object.
+        """
+        self.__size = size

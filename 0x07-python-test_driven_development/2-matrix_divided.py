32c02f14a0b2eabe486ffb6cafbc024c057b7865
A-fethi <fethiabderrahmane1@gmail.com>
2023-07-01 06:53:06 -0700

    adding matrix_divided and it's doctest

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    message = "matrix must be a matrix (list of lists) of integers/floats"
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for rows in matrix:
+        if len(rows) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for i in row:
+            if not isinstance(i, (int, float)):
+                raise TypeError(message)
+            else:
+                new_row.append(round(i / div, 2))
+        new_matrix.append(new_row)
+    return new_matrix

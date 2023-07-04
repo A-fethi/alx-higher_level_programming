e2d49e81f61739ee9dc9f8574274335eb49ac589
A-fethi <fethiabderrahmane1@gmail.com>
2023-07-04 06:58:01 -0700

    __slots__ attribute to restrict the creation of attributes except a specific attribute name

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define lockedclass."""
+
+
+class LockedClass:
+    """
+    prevents the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_name.
+    """
+    __slots__ = ["first_name"]

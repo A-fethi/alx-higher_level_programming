aa5cec68a6a2770bb81c938f16885bcd98c88b26
A-fethi <fethiabderrahmane1@gmail.com>
2023-07-05 07:37:34 -0700

    fix the code

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,18 +8,22 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+
     index = 0
-    for i in text:
-        if i in " .?:":
-            if text[index + 1] == " ":
+    while index < len(text):
+        if text[index] in ".:?":
+            if index + 1 < len(text) and text[index + 1] == " ":
                 text = text[:index + 1] + text[index + 2:]
+            else:
+                index += 1
         else:
             index += 1
+
     index = 0
-    for i in text:
-        if i in " .?:":
+    while index < len(text):
+        if text[index] in ".:?":
             text = text[:index + 1] + '\n\n' + text[index + 1:]
-            index += 3
+            index += 4
         else:
             index += 1
     print(text, end="")

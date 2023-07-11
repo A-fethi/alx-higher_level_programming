44cce63422025ec14e44bf3b349c8633171185b4
A-fethi <fethiabderrahmane1@gmail.com>
2023-07-11 04:52:17 -0700

    fix requirements

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Define class_to_json function."""
-import json
 
 
 def class_to_json(obj):
@@ -9,4 +8,4 @@ def class_to_json(obj):
     (list, dictionary, string, integer and boolean)
     for JSON serialization of an object.
     """
-    return json.dumps(obj.__dict__)
+    return obj.__dict__

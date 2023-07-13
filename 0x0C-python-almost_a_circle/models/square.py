ce4cbba63311c774903d0a5b803bcaed6a1521f1
A-fethi <fethiabderrahmane1@gmail.com>
2023-07-13 10:25:12 -0700

    Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,4 +25,19 @@ class Square(Rectangle):
 
     def __str__(self):
         """Returns a string presentation of square object."""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.__width}"
+
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute."""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)

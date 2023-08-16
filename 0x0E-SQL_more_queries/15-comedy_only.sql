6d6a29f94e5398a8761f1ce1cab6824bd1399be3
A-fethi <fethiabderrahmane1@gmail.com>
2023-08-16 10:40:13 -0700

    Only Comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- a script that lists all shows contained in the database hbtn_0d_tvshows
+SELECT tv_shows.title
+FROM tv_show_genres
+LEFT JOIN tv_genres
+ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genre_name = "Comedy"
+ORDER BY tv_shows.title ASC;

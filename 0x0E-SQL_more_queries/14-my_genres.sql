deec8bd3c421376b972620fcef2976f96b146989
A-fethi <fethiabderrahmane1@gmail.com>
2023-08-16 10:32:02 -0700

    My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- a script that lists all shows contained in the database hbtn_0d_tvshows
+SELECT tv_genres.name
+FROM tv_show_genres
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC;

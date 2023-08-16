d6a66bff96e09b18dc52c7e15014f2f828feb3bd
A-fethi <fethiabderrahmane1@gmail.com>
2023-08-16 08:37:45 -0700

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,6 @@
 -- a script that lists all cities contained in the database hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name
-FROM cities, states
+FROM cities
+JOIN states
+ON cities.state_id = states.id
 ORDER BY id ASC;

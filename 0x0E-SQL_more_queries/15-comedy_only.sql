29d756b48997f6b665e2cac1ccab620e6a1723dc
A-fethi <fethiabderrahmane1@gmail.com>
2023-08-16 11:21:44 -0700

    Only Comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,8 +2,8 @@
 SELECT tv_shows.title
 FROM tv_show_genres
 LEFT JOIN tv_shows
-ON tv_show_genres.show_idi = tv_shows.id
+ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres_name = "Comedy"
+WHERE tv_genres.name = "Comedy"
 ORDER BY tv_shows.title ASC;

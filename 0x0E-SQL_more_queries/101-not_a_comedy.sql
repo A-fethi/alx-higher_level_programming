9babd0ded6611b7b4292e81274078373374007ad
A-fethi <fethiabderrahmane1@gmail.com>
2023-08-16 13:44:07 -0700

    No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -5,7 +5,7 @@ WHERE tv_shows.title NOT IN (
 	SELECT tv_shows.title
 	FROM tv_show_genres
 	LEFT JOIN tv_shows
-	ON tv_show_genres.genre_id = tv_shows.id
+	ON tv_show_genres.show_id = tv_shows.id
 	JOIN tv_genres
 	ON tv_show_genres.genre_id = tv_genres.id
 	WHERE tv_genres.name = "Comedy"

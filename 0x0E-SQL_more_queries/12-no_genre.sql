2aeecb0268223b1e5363d57a3b0c228821d2a8f1
A-fethi <fethiabderrahmane1@gmail.com>
2023-08-16 09:48:46 -0700

    No genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
 -- a script that lists all shows contained in the database hbtn_0d_tvshows
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-RIGHT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
+FROM tv_show_genres
+RIGHT JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;

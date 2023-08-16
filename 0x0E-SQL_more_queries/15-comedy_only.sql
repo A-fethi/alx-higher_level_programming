07959e0e115e58c0b53033ab085a6ae5dcc6b264
A-fethi <fethiabderrahmane1@gmail.com>
2023-08-16 10:42:05 -0700

    Only Comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- a script that lists all shows contained in the database hbtn_0d_tvshows
 SELECT tv_shows.title
 FROM tv_show_genres
-LEFT JOIN tv_genres
+LEFT JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id

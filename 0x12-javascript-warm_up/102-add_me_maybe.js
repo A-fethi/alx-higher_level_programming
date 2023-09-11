e2108365e5d487e2c31cd95f2065bd079534ecb5
A-fethi <fethiabderrahmane1@gmail.com>
2023-09-11 11:11:18 -0700

    incrementing and calling a function

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 exports.addMeMaybe = function (number, theFunction) {
-  let i = 0;
-  while (++i < number) {
-    i++;
-  }
-  theFunction(i);
+  number++;
+  theFunction(number);
 };

7e24b6a22a9b9404ac0dfa01403e6e704324c19c
A-fethi <fethiabderrahmane1@gmail.com>
2023-09-12 10:01:37 -0700

    importing an array and computing a new array

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 console.log(list);
-const map1 = list.map((x) => x * (x - 1));
+const map1 = list.map((x, index) => x * index);
 console.log(map1);

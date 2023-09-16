c4bf60b53c17e2f00089527cba5ec01eccd49e02
A-fethi <fethiabderrahmane1@gmail.com>
2023-09-16 04:51:45 -0700

    fixing requirements

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,8 +3,9 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
+
 class City(Base):
-    """State class"""
+    """"City class representing the cities table in the database."""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)

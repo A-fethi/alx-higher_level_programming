0ff2fe6799c106c6d66d1c68d37f1196b760c3be
A-fethi <fethiabderrahmane1@gmail.com>
2023-09-29 04:58:26 -0700

    fetching url using urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import urllib.request
+"""Python script that fetches https://alx-intranet.hbtn.io/status
+using the package urllib"""
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")

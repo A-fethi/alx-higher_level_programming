75d9aabe4c9c9d805f82c4086eb4311cbd29d877
A-fethi <fethiabderrahmane1@gmail.com>
2023-09-29 10:13:40 -0700

    Error code using requests package

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL
+and displays the body of the response."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    if r.status_code < 400:
+        print(r.text)
+    else:
+        print(f"Error code: {r.status_code}")

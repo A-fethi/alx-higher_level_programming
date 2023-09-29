e4cd889f23411e58e207e11676eb9b264589a854
A-fethi <fethiabderrahmane1@gmail.com>
2023-09-29 09:58:15 -0700

    sending a POST request using requests package

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+    r = requests.post(url, data=value)
+    print(r.text)
